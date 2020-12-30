868d917414c5dd56005cb62e0a4697757d83fdec
Diane Marino <dianemarino0@gmail.com>
2020-12-30 11:43:13 +0100

    config tailwind delete bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,6 +2,7 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
+import '../stylesheets/application.scss';
 
 require("@rails/ujs").start()
 require("turbolinks").start()
@@ -23,7 +24,7 @@ require("channels")
 // ----------------------------------------------------
 
 // External imports
-import "bootstrap";
+// import "bootstrap";
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
